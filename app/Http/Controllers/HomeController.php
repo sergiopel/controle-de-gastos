22432951bdb196acbf9dfa06787a73bb1d87e86f
Sergio Pelegrini <serpel@gmail.com>
2025-03-17 17:32:00 -0300

    fix: corrige cor e saldo das receitas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Expense;
+use App\Models\Income;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,8 +12,13 @@ class HomeController extends Controller
     public function index()
     {
         $totalUsers = User::count();
+        
         $totalExpenses = Expense::sum('amount');
         $totalExpenses = number_format($totalExpenses, 2, ',', '.');
-        return view('home', compact('totalUsers', 'totalExpenses'));
+        
+        $totalIncomes = Income::sum('amount');
+        $totalIncomes = number_format($totalIncomes, 2, ',', '.');
+        
+        return view('home', compact('totalUsers', 'totalExpenses', 'totalIncomes'));
     }
 } 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,14 +34,11 @@ $breadcrumbs = [
             <!--begin::Small Box Widget 2-->
             <div class="small-box text-bg-success">
                 <div class="inner">
-                    <h3>53<sup class="fs-5">%</sup></h3>
-                    <p>Despesas</p>
+                    <h3>{{ $totalIncomes }}</h3>
+                    <p>Receitas</p>
                 </div>
-                <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true">
-                    <path
-                        d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z">
-                    </path>
+                <svg class="small-box-icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
                 </svg>
                 <a href="#"
                     class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover">
