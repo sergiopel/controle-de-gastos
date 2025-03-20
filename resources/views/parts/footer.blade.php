dd9a72b28ab1050f34260564f641675dcd905777
Sergio Pelegrini <serpel@gmail.com>
2025-03-20 15:30:25 -0300

    fix: corrige informações do rodapé

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,14 +1,14 @@
         <!--begin::Footer-->
         <footer class="app-footer">
             <!--begin::To the end-->
-            <div class="float-end d-none d-sm-inline">Anything you want</div>
+            {{-- <div class="float-end d-none d-sm-inline">Anything you want</div> --}}
             <!--end::To the end-->
             <!--begin::Copyright-->
             <strong>
-                Copyright &copy; 2014-2024&nbsp;
-                <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
+                Copyright &copy; 2025&nbsp;
+                <a href="#" class="text-decoration-none">ProjetosDev</a>
             </strong>
-            All rights reserved.
+            Todos os direitos reservados.
             <!--end::Copyright-->
         </footer>
         <!--end::Footer-->
